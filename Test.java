17c6dc504809b81451297b3494fdea4a97db61b0
usanci <33079317+usanci@users.noreply.github.com>
2019-01-24 17:12:41 +0800

    Testing
    Testing github

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+		
+	public static void main(String[] args) {
+		
+		long startTime = System.currentTimeMillis();
+    System.out.println("userTime:"+(System.currentTimeMillis() - startTime)+"ms");
+
+}
+}
